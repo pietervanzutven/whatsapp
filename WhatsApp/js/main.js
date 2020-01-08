858cc70ce121244713a1519ee47e0367d1da1daa
Pieter van Zutven <pietervanzutven@outlook.com>
2020-01-08 20:11:42 +0100

    Add project files.

---
+++ WhatsApp/js/main.js
@@ -0,0 +1,8 @@
+﻿'use strict';
+
+window.onload = () => {
+    var userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.45 Safari/537.36 Edg/79.0.309.30";
+    var httpRequestMessage = new Windows.Web.Http.HttpRequestMessage(Windows.Web.Http.HttpMethod.get, new Windows.Foundation.Uri('https://web.whatsapp.com/'));
+    httpRequestMessage.headers.append("User-Agent", userAgent);
+    webView.navigateWithHttpRequestMessage(httpRequestMessage);
+}
