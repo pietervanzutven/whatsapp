76847a319be0db786c76a2d502f961f1a2c59c81
Pieter van Zutven <pietervanzutven@outlook.com>
2020-04-05 21:28:29 +0200

    Temporary fix for transparent backgrounds

--- WhatsApp/js/main.js
+++ WhatsApp/js/main.js
@@ -23,9 +23,31 @@ window.onload = () => {
         webView.invokeScriptAsync('eval',
             'var style = document.createElement("style");' +
             'style.appendChild(document.createTextNode("' + 
-                '.-peIt { min-width:0; }' +
-                '@media screen and (max-width:648px) { .h70RQ { min-width:0; } }' +
-                '@media screen and (max-width:660px) { .landing-wrapper { min-width:0; } }' +
+                '._2dA13 { min-width:0; }' +
+                '@media screen and (max-width:648px) { .h70RQ { min-width: 0; } }' +
+                '@media screen and (max-width:660px) { .landing-wrapper { min-width: 0; } }' +
+                'html[dir] ._2fq0t { background-color: #f7f7f7; }' +
+                'html[dir] ._1CkkN { background-color: 3fff; }' +
+                'html[dir] ._1NVPn { background-color: #ededed;}' +
+                'html[dir] ._3auIg { background-color: #ededed; }' +
+                'html[dir] ._35DHA { background-color: #9de1fe; }' +
+                'html[dir] .rRAIq { background-color: #f7f7f7; }' +
+                'html[dir] ._1RQfk { background-color: #fff; }' +
+                'html[dir] ._1FroB { background-color: #00bfa5 }' +
+                'html[dir] ._1AKfk { background-color: #fff; }' +
+                'html[dir] ._2EXPL { background-color: #fff; }' +
+                'html[dir] .Zq3Mc { background-color: rgba(225, 245, 254, 0.92); }' +
+                'html[dir] ._2y17h { background-color: #ededed; }' +
+                'html[dir] .message-in .MVjBr { background-color: #fff; }' +
+                'html[dir] .message-out .MVjBr { background-color: #dcf8c6; }' +
+                'html[dir] ._298R6 { background-color: #fff; }' +
+                'html[dir] ._3pkkz { background-color: #f0f0f0; }' +
+                'html[dir] ._1Plpp { background-color: #fff; }' +
+                'html[dir] ._1CSx9 { background-color: #ededed; }' +
+                'html[dir] ._1CRb5 { background-color: #fff; }' +
+                'html[dir] ._1qUma { background-color: #f0f0f0; }' +
+                'html[dir] ._3tlsa { background-color: #f0f0f0; }' +
+                'html[dir] ._1CnF3 { background-color: #fff; }' +
             '"));' +
             'document.head.appendChild(style);' +
             'var interval = setInterval(() => {' +
