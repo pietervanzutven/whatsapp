8e554fc2c46595293be70f4ac9ebacc14b9fa075
Pieter van Zutven <pietervanzutven@outlook.com>
2020-09-30 11:32:49 +0200

    Enable dark theme and system default option. Requires app restart for changes to take effect.

--- WhatsApp/js/main.js
+++ WhatsApp/js/main.js
@@ -2,6 +2,7 @@
 
 var currentView = Windows.UI.Core.SystemNavigationManager.getForCurrentView();
 var appViewBackButtonVisibility = Windows.UI.Core.AppViewBackButtonVisibility;
+var viewManagement = Windows.UI.ViewManagement;
 
 var gutter = "var gutter = document.getElementsByClassName('_1-iDe _1xXdX'); gutter.length > 0 && (gutter[1].style.display = ";
 var conversation = "var conversation = document.getElementsByClassName('_1-iDe Wu52Z'); conversation.length > 0 && (conversation[1].style.display = ";
@@ -12,6 +13,12 @@ window.onload = () => {
     webView.addEventListener("MSWebViewContentLoading", () => webView.invokeScriptAsync("eval", "delete window.Headers;").start());
 
     webView.addEventListener("MSWebViewNavigationCompleted", () => {
+        var background = viewManagement.UISettings().getColorValue(viewManagement.UIColorType.background);
+        if (background.r === 255) {
+            var systemTheme = "light";
+        } else {
+            var systemTheme = "dark";
+        }
         webView.invokeScriptAsync("eval",
             "var style = document.createElement('style');" +
             "style.appendChild(document.createTextNode('" +
@@ -22,6 +29,11 @@ window.onload = () => {
                 "@media screen and (max-width:600px) { ._1-iDe {position: absolute; width: 100%; } }" +
             "'));" +
             "document.head.appendChild(style);" +
+            "var systemTheme = window.localStorage.getItem('system-theme-mode');" +
+            "var theme = systemTheme === 'true' ? '\"" + systemTheme + "\"' : window.localStorage.getItem('theme');" +
+            "if (theme === '\"dark\"') {" +
+                "document.body.classList.add('dark');" +
+            "}" +
             "var variables = {};" +
             "var styleSheets = document.styleSheets;" +
             "for (var i=0; i<styleSheets.length; i++) {" +
@@ -30,8 +42,8 @@ window.onload = () => {
                     "var rules = styleSheet.rules;" +
                     "for (var j=0; j<rules.length; j++) {" +
                         "var rule = rules[j];" +
-                        "if (rule.cssText.includes(':root { ')) {" +
-                            "var declarations = rule.cssText.replace(':root { ','').replace('}','').split('; ');" +
+                        "if ((theme === '\"light\"' && rule.cssText.startsWith(':root { ')) || (theme === '\"dark\"' && rule.cssText.startsWith('.dark { '))) {" +
+                            "var declarations = rule.cssText.replace(':root { ','').replace('.dark { ','').replace('}','').split('; ');" +
                             "for (var k=0; k<declarations.length; k++) {" +
                                 "var declaration = declarations[k];" +
                                 "if (declaration) {" +
