77dc54675d6f4975edd5d6e14508620f7f900ccc
Pieter van Zutven <pietervanzutven@outlook.com>
2021-10-13 21:41:18 +0200

    Add simple navigation interface

--- WhatsApp/js/main.js
+++ WhatsApp/js/main.js
@@ -6,7 +6,7 @@ window.Buffer = require('buffer').Buffer;
 
 async function connectToWhatsApp(conn) {
     const authInfo = JSON.parse(Windows.Storage.ApplicationData.current.localSettings.values["authInfo"]);
-    
+
     conn.on("qr", qr => {
         QR.generate(qr, { small: true }, function (qrcode) {
             const div = document.getElementById("qr");
@@ -20,10 +20,15 @@ async function connectToWhatsApp(conn) {
     });
 
     conn.on("contacts-received", () => {
-        const element = document.getElementById('contacts');
         conn.chats.array.forEach(chat => {
-            element.innerHTML += chat.name + '\n';
+            const link = document.createElement("a");
+            link.addEventListener("click", () => openConversation(chat.jid));
+            link.href = "#";
+            link.innerHTML = chat.name + "\n";
+            contacts.appendChild(link);
         });
+        contacts.classList.remove("hidden");
+        qr.classList.add("hidden");
     });
 
     if (authInfo) {
@@ -38,8 +43,41 @@ async function connectToWhatsApp(conn) {
     }
 }
 
+function openConversation(jid) {
+    contacts.classList.add("hidden");
+    conversation.classList.remove("hidden");
+
+    const envelopes = conn.chats.dict[jid].messages.array;
+    conversation.innerHTML = "";
+    envelopes.forEach(envelope => {
+        const message = envelope.message;
+        if (message) {
+            if (message.conversation) {
+                const div = document.createElement("div");
+                div.innerHTML = message.conversation;
+                div.classList.add("message");
+                if (envelope.key.fromMe) {
+                    div.classList.add("right");
+                } else {
+                    div.classList.add("left");
+                }
+                conversation.appendChild(div);
+            }
+        }
+    });
+
+    Windows.UI.Core.SystemNavigationManager.getForCurrentView().appViewBackButtonVisibility = Windows.UI.Core.AppViewBackButtonVisibility.visible;
+    Windows.UI.Core.SystemNavigationManager.getForCurrentView().onbackrequested = event => {
+        event.handled = true;
+        conversation.classList.add("hidden");
+        contacts.classList.remove("hidden");
+        Windows.UI.Core.SystemNavigationManager.getForCurrentView().appViewBackButtonVisibility = Windows.UI.Core.AppViewBackButtonVisibility.collapsed;
+    }
+}
+
+let conn;
 window.onload = () => {
-    const conn = new WAConnection();
+    conn = new WAConnection();
 
     connectToWhatsApp(conn);
 }
